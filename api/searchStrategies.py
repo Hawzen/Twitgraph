31b3d4d773ad030a47ad8f04eebea7197127c771
Hawzen <hawzen@users.noreply.github.com>
2022-01-24 19:09:23 +0300

    Fix commented code when adding tqdm

--- api/searchStrategies.py
+++ api/searchStrategies.py
@@ -13,16 +13,16 @@ def followersStrategy(screenName: str, graph: Graph, api: tweepy.API, nIteration
     try:
         for i in tqdm(range(nIterations)):
             graph.idSearch_graph(api)
-            # if graph.getNodeNum() - graph.getDoneNum() <= 15:
-            #     graph.mopSearch(api)
+            if graph.getNodeNum() - graph.getDoneNum() <= 15:
+                graph.mopSearch(api)
 
-            # saveShelve(screenName, graph, dump=dump, onlyDone=True)
-            # if not quiet: print(f"\nFinished {i + 1} iteration.\n\tTotal number of nodes: {graph.getNodeNum()}\
-            #                             \n\tTotal number of done nodes: {graph.getDoneNum()}\n")
+            saveShelve(screenName, graph, dump=dump, onlyDone=True)
+            if not quiet: print(f"\nFinished {i + 1} iteration.\n\tTotal number of nodes: {graph.getNodeNum()}\
+                                        \n\tTotal number of done nodes: {graph.getDoneNum()}\n")
 
-            # for minute in range(15):
-            #     if not quiet: print("\r░ MINUTES UNTIL NEXT BATCH\t{:>02}".format(15 - minute), end=" ")
-            #     sleep(60)
+            for minute in range(15):
+                # if not quiet: print("\r░ MINUTES UNTIL NEXT BATCH\t{:>02}".format(15 - minute), end=" ")
+                sleep(60)
             sleep(15)
             # if not quiet: print("\r░ MINUTES UNTIL NEXT BATCH\t{:>02}".format(0), end=" ")
     finally:
