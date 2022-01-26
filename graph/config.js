d3debbe863b035da93582b3db5a9812c8240b1cc
Hawzen <43524721+Hawzen@users.noreply.github.com>
2022-01-26 11:39:28 -0800

    Update config.js

--- graph/config.js
+++ graph/config.js
@@ -1,40 +1,40 @@
 let config =
 {
-        "constants": {
-                "nodeStretch": 0.5,
-                "edgeSize": 1,
-                "clusterSize": 1,
-                "nodeSize": 2,
-                "minNodeSize": 3,
-                "maxNodeSize": 15,
-                "minEdgeSize": 0.1,
-                "maxEdgeSize": 10,
-                "maxProfiles": 5
-        },
-        "coloring": {
-                "inactive": "rgba(110, 154, 66, 0.2)",
-                "active": "rgba(110, 154, 66, 0.8)",
-                "edge": "rgba(71, 104, 37, 0.05)",
-                "inactiveEdge": "rgba(135, 121, 82, 0.03)",
-                "activeEdge": "rgb(76, 120, 32)",
-                "origin": "#FFD700",
-                "clusterActive": "rgb(83, 141, 26)",
-                "clusterInactive": "rgba(83, 141, 26, 0.2)",
-                "clusterEdge": "rgba(134, 154, 66, 1)",
-                "clusterInactiveEdge": "rgba(134, 154, 66, 0.3)",
-                "protectedCluster": "rgb(110, 154, 66)",
-                "button-background": "rgba(83, 141, 26, 0.5)",
-                "button-color": "rgba(41, 70, 13)",
-                "background": "rgb(44, 103, 5, 0.2)",
-                "miniMapBackground": "rgb(44, 103, 5, 0)",
-                "box": "rgba(41, 70, 13, 0.2)",
-                "boxText": "rgba(0, 0, 0, 0.95)",
-                "boxTextShadow": "rgb(44, 103, 5, 0.2)",
-                "labelColor": "black"
-        },
-        "layout": "forceDirectedLayout",
-        "edges": {
-                "nodeEdges": "line",
-                "clusterEdges": "dashed"
-        }
+    "constants": {
+        "nodeStretch": 0.5,
+        "edgeSize": 1,
+        "clusterSize": 1,
+        "nodeSize": 2,
+        "minNodeSize": 3,
+        "maxNodeSize": 15,
+        "minEdgeSize": 0.1,
+        "maxEdgeSize": 10,
+        "maxProfiles": 5
+    },
+    "coloring": {
+        "inactive": "rgba(11, 19, 29, 0.2)",
+        "active": "rgb(94, 129, 172)",
+        "edge": "rgba(47, 71, 94, 0.06)",
+        "inactiveEdge": "rgba(47, 71, 94, 0.03)",
+        "activeEdge": "#88C0D0",
+        "origin": "#FFD700",
+        "clusterActive": "rgba(143, 188, 187, 1)",
+        "clusterInactive": "rgba(143, 188, 187, 0.2)",
+        "clusterEdge": "rgba(68, 137, 222, 1)",
+        "clusterInactiveEdge": "rgba(68, 137, 222, 0.2)",
+        "protectedCluster": "#A5E2D2",
+        "button-background": "rgba(143, 187, 188, 0.8)",
+        "button-color": "rgba(47, 74, 75, 1)",
+        "background": "#2E445B",
+        "miniMapBackground": "rgb(46, 68, 100, 0)",
+        "box": "rgba(143, 187, 188, 0.8)",
+        "boxText": "black",
+        "boxTextShadow": "rgb(143, 188, 187)",
+        "labelColor": "white"
+    },
+    "layout": "forceDirectedLayout",
+    "edges": {
+        "nodeEdges": "line",
+        "clusterEdges": "dashed"
+    }
 }
